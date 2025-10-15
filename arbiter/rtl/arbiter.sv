5d55778d82c5996578065c018a7ea08b94a66d8a
Julin Shaji <julinshaji01@gmail.com>
2025-10-15 12:49:52 +0530

    reorganize dirs

---
+++ arbiter/rtl/arbiter.sv
@@ -0,0 +1,70 @@
+`timescale 1ns/1ps
+
+// `include "arbiter_if.sv"
+
+module arbiter #(parameter int COUNT = 4) (
+    arbiter_if.dut arb_if
+);
+
+    typedef enum logic [1:0] {
+        IDLE,
+        BUSY
+    } state_t;
+
+    // Assumptions:
+    // - Only one IP has access at a time
+    // - Round-robin
+
+    logic [$clog2(COUNT)-1:0] cur_idx;
+    logic [COUNT-1:0] nxt_idx;
+    state_t state;
+
+    always_comb begin
+        // Avoid inferred latch
+        nxt_idx = '0;
+
+        for(int i=0; i<COUNT; ++i) begin
+            automatic int idx = (cur_idx + i + 1) %  COUNT;
+            if(arb_if.req[idx]) begin
+                nxt_idx = idx;
+                break;
+            end
+        end
+    end
+
+    always_ff @(posedge arb_if.clk or posedge arb_if.rst) begin
+        if (arb_if.rst) begin
+            arb_if.cur_idx <= '0;
+            //nxt_idx <= '0;
+            state <= IDLE;
+        end else begin
+            unique case(state)
+                IDLE: begin
+                    if (|arb_if.req) begin
+                        // At least one request access
+                        arb_if.grt <= nxt_idx;
+                        cur_idx <= $clog2(nxt_idx);
+                        state <= BUSY;
+                    end else begin
+                        // No one needs access
+                        arb_if.grt <= '0;
+                    end
+                end
+                BUSY: begin
+                    if (|((arb_if.grt >> cur_idx) & 1)) begin
+                        if(!(|nxt_idx)) begin
+                            // No other requests
+                            state <= IDLE;
+                        end else begin
+                            arb_if.grt <= nxt_idx;
+                            cur_idx <= $clog2(nxt_idx);
+                        end
+                    end
+                end
+                /* default: begin */
+                /*     state <= IDLE; */
+                /* end */
+            endcase
+        end
+    end
+endmodule
---
+++ arbiter/rtl/arbiter_if.sv
@@ -0,0 +1,38 @@
+`timescale 1ns/1ps
+
+interface arbiter_if #(
+    parameter COUNT = 2
+);
+
+    logic clk;
+    logic rst;
+    // logic [COUNT-1:0] lock;
+    logic [COUNT-1:0] req;   // requests
+    logic [COUNT-1:0] ack;   // acknowledgement
+
+    logic [COUNT-1:0] grt;   // grant
+
+    modport dut (
+        input clk,
+        input rst,
+        input req,
+        input ack,
+        output grt
+    );
+
+    /* modport tb ( */
+    /*     output req, */
+    /*     input grt, */
+    /*     input clk, rst */
+
+    /*     input clk, */
+    /*     input rst, */
+    /*     input req, */
+    /*     input ack, */
+    /*     output grt */
+    /* ); */
+
+endinterface
+
+
+
